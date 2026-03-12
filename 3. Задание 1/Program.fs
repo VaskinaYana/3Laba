c6740513c2162455cb5d131f6753cf9be94f5065
Yanaa <vaskinayana866@gmail.com>
2026-03-12 21:53:54 +0500

    3Laba

---
+++ 3. Задание 1/Program.fs
@@ -0,0 +1,37 @@
+﻿[<EntryPoint>]
+let main argv =
+    printfn "Введите числа от 1 до 9 (для завершения введите любой другой символ):"
+
+    let rec read () =
+        seq {
+            let input = System.Console.ReadLine()
+
+            match System.Int32.TryParse(input) with
+            | (true, number) when number >= 1 && number <= 9 ->
+                yield number
+                yield! read ()
+            | _ -> ()
+        }
+
+    let numbers = read ()
+
+    let Roman number =
+        match number with
+        | 1 -> "I"
+        | 2 -> "II"
+        | 3 -> "III"
+        | 4 -> "IV"
+        | 5 -> "V"
+        | 6 -> "VI"
+        | 7 -> "VII"
+        | 8 -> "VIII"
+        | 9 -> "IX"
+        | _ -> ""
+
+    let RomanNumbers =
+        numbers
+        |> Seq.map Roman
+
+    printfn "Результат: %A" (RomanNumbers |> Seq.toList)
+
+    0
---
+++ 3. Задание 3/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите путь к каталогу:"
+
+    let path = Console.ReadLine()
+
+    if not (Directory.Exists(path)) then
+        printfn "Каталог не существует."
+    else
+        let files =
+            Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories)
+            |> Seq.map Path.GetExtension
+
+        let mostOften =
+            files
+            |> Seq.groupBy id
+            |> Seq.map (fun (ext, group) -> ext, Seq.length group)
+            |> Seq.maxBy snd
+
+        printfn "Наиболее часто встречающееся расширение: %s" (fst mostOften)
+        printfn "Количество файлов: %d" (snd mostOften)
+
+    0
