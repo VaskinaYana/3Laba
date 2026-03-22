f50b082bd5e531d0c35f618628bfdb58c6d792e4
VaskinaYana <vaskinayana866@gmail.com>
2026-03-23 03:23:24 +0500

    Add files via upload

---
+++ 3 .Задание 2/Program.fs
@@ -0,0 +1,25 @@
+﻿let rec readDigits () =
+    seq {
+        let input = System.Console.ReadLine()
+
+        match System.Int32.TryParse(input) with
+        | (true, digit) when digit >= 0 && digit <= 9 ->
+            yield digit
+            yield! readDigits ()
+        | _ -> ()
+    }
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите десятичные цифры (для завершения любой другой символ):"
+
+    let originaldigits = readDigits ()
+
+    let numberAsString =
+        originaldigits
+        |> Seq.map string
+        |> String.concat ""
+
+    printfn "Полученное число: %s" numberAsString
+    
+    0
